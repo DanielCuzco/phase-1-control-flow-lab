0aa9f42f8da6ca96c5eefbde692b26ec0a107338
Daniel <daniel.cuzco@gmail.com>
2022-06-28 21:25:46 -0400

    commit tree 32731cc7ccd0eb2117c311320ef709ce98ca6655

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  // Write your code here!
+  let ride;
+  if( feet < 400) {
+    ride = "This one is on me!";
+    return ride 
+  }
+  else if(feet >= 400 && feet <= 2000) {
+    ride = "That will be twenty bucks.";
+    return ride;
+  }
+   else if(feet > 2000 && feet <= 2500) {
+    ride = "I will gladly take your thirty bucks.";
+    return ride;
+  }
+  else(feet > 2500) 
+    ride = "No can do."
+    return ride;
+
+
+
+}
+
+function ternaryCheckCity(city){
+  // if(city === "NYC") {
+    // return "Ok, sounds good.";
+  // }
+    // else{
+      // return "No go.";
+    // }
+    return (city === "NYC") ? "Ok, sounds good." : "No go."
+
+
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tipString) {
+  switch(tipString) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    
+    }
+
 }
